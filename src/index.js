1949f34c6b51f99112e886b6d054e71dbba3579d
CuppaKe <turnman87@gmail.com>
2018-09-29 19:12:14 +0300

    updated syntax and comments

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
+/**
+ * Function solves quadratic equation
+ * @param {string} equation - quadratic equation
+ * @return {array} solutions as ordered array
+ */
 module.exports = function solveEquation(equation) {
-
-	let arr = equation.replace(/\s/g, '').match(/(\+|\-|\d)\d{1,}/g);
-	let a = +arr[0];
-	let b = +arr[1];
-	let c = +arr[2];
-
-	let disc = Math.pow(b, 2) - 4 * a * c;
-	let x1 = Math.round((-b + Math.sqrt(disc)) / (2 * a));
-	let x2 = Math.round((-b - Math.sqrt(disc)) / (2 * a));
-
-	return [x1,x2].sort((a, b) => a - b);
-	
-}
+  const arr = equation.replace(/\s/g, '').match(/(\+|\-|\d)\d{1,}/g);
  
+  // ax^2+bx+c=0
+ 
+  const a = +arr[0];
+  const b = +arr[1];
+  const c = +arr[2];
 
+  const disc = Math.pow(b, 2) - 4 * a * c;
+  const x1 = Math.round((-b + Math.sqrt(disc)) / (2 * a));
+  const x2 = Math.round((-b - Math.sqrt(disc)) / (2 * a));
+
+  return [x1, x2].sort((x, y) => x - y);
+};
